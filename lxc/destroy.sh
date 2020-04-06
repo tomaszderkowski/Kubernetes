d9ebcc86ceefe05adf3eb5fc5a73609db20c001b
Tomasz Derkowski <derkowskitomasz@gmail.com>
2020-04-06 18:34:14 +0200

    Remove LXC configuration

--- lxc/destroy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-lxc delete kubernetes-master --force                               
-lxc delete kubernetes-worker1 --force                               
-lxc delete kubernetes-worker2 --force 
--- lxc/prepare.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-yum update 
-yum -y install openssh-server
-systemctl enable sshd
-systemctl start sshd
-echo "asdzxc" | passwd --stdin root
